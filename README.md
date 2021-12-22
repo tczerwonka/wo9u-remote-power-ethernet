87fe9398493f407883fbc9bae84924d3215480d3
T Czerwonka <tczerwonka@gmail.com>
2021-12-21 20:37:08 -0600

    initial commit of sample code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # wo9u-remote-power-ethernet
+
+Various dumb things to remote control power via ethernet using a
+ETAPD11SS and arduino ethernet shield on a private network.
---
+++ remote-power-v1/remote-power-v1.ino
@@ -0,0 +1,117 @@
+
+//////////////////////////////////////////////////////////////////////////
+//arduino remote power v1
+//starting with sample code from here
+//https://www.arduino.cc/en/Tutorial/LibraryExamples/WebServer
+//////////////////////////////////////////////////////////////////////////
+// code will eventually need...
+//   -- some sort of status page
+//   -- ability to turn an output on/off for power control
+//   -- momentary / addressable output control
+//   -- environmental monitoring?
+//   -- unit status report?
+//////////////////////////////////////////////////////////////////////////
+
+
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0x00, 0x01
+};
+IPAddress ip(192, 168, 1, 80);
+EthernetServer server(80);
+
+//////////////////////////////////////////////////////////////////////////
+//setup
+//////////////////////////////////////////////////////////////////////////
+void setup() {
+  // You can use Ethernet.init(pin) to configure the CS pin
+  Ethernet.init(10);  // Most Arduino shields
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Ethernet WebServer Example");
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start the server
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//loop
+//////////////////////////////////////////////////////////////////////////
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+
+}
